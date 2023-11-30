1ca5282f63cfb777585efb692f7021a27d3b4e34
Mike Gusev <mihail.gusiev@gmail.com>
2023-12-01 00:09:54 +0200

    Fix

--- src/generic/4.ts
+++ src/generic/4.ts
@@ -1,6 +1,10 @@
 /*
   Використовуйте generics та інтерфейси, щоб виправити помилку в наступних класах:
 */
+interface Title {
+  title: string
+}
+
 
 class Component<T> {
   constructor (public props:T) {
@@ -8,7 +12,7 @@ class Component<T> {
   }
 }
 
-class Page extends Component<{ title: string }> {
+class Page extends Component<Title> {
   pageInfo() {
     console.log(this.props.title);
   }
